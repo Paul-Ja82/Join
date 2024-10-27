1826977276aae3783a94d88c9dd298978fe72524
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-10-27 08:45:55 +0100

    kleine Erweiterungen

--- js/board.js
+++ js/board.js
@@ -1,11 +1,32 @@
 function openTaskOverlay(e) {
-    document.getElementById(`task_overlay_ctn`).style.display = "flex";
+    document.getElementById(`task_overlay_ctn`).style.right = "0";
+    document.body.style.overflow = "hidden"; 
 }
 
 function closeTaskOverlay(e) {
     if (e.target.id === "task_overlay_ctn" || e.target.id === "close_task_overlay" || e.target.id === "close_task_overlay_svg") {
         console.log(e.target.id)
         console.log(e)
-        document.getElementById("task_overlay_ctn").style.display = "none";
+        document.getElementById(`task_overlay_ctn`).style.right = "-100%";
     }
+     document.body.style.overflow = "";
+}
+
+let currentDraggedElement;
+
+function startDragging(id) {
+    currentDraggedElement = `single_task_ctn${id}`;
+    console.log(currentDraggedElement)
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function moveTo(category) {
+ document.getElementById(category).innerHTML += currentDraggedElement
+}
+
+function openAddTaskForm() {
+    window.location.href = "/add_task.html"
 }
