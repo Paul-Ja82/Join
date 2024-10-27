00d1def58cf9a5377ae1b16bbae03ce806ce1a1f
MetalRick <lernen@reinitestet.at>
2024-10-27 07:39:26 +0100

    add addContactButton-Handlers

--- js/contact.js
+++ js/contact.js
@@ -7,6 +7,7 @@ mediaDesktop.addEventListener('change', mediaChangeHandler);
 
 function initContact() {
     console.log('intitContact()'); ///DEBUG
+    initForms();
     include();
     mediaChangeHandler();
     addListItemClickHandlers();
@@ -48,6 +49,60 @@ function setInfo() {
     console.log('setInfo()'); ///DEBUG
 }
 
+/*#################*/
+/*## ADD CONTACT ##*/
+/*#################*/
+
+function addContactButtonHandler() {
+    clearAddContactForm();
+    setContactDialogAdd();
+    resetVsmgs('contactForm');
+    openDialog('dialogContact');
+}
+
+function clearAddContactForm() {
+    let nameInputElem= document.getElementById('nameInputElem');
+    let emailInputElem= document.getElementById('emailInputElem');
+    let phoneInputElem= document.getElementById('phoneInputElem');
+    nameInputElem.value= '';
+    emailInputElem.value= '';
+    phoneInputElem.value= '';
+}
+
+function setContactDialogAdd() {
+    let headline= document.getElementById('CDheadline');
+    headline.innerHTML= 'Add contact';
+    showElem('CDtagline');
+    cdSetPersonIconAdd();
+    setFormAdd();
+}
+
+function cdSetPersonIconAdd() {
+    let personIcon= document.getElementById('cdPersonIcon');
+    personIcon.style.backgroundColor= '#D1D1D1';
+    hideElem('cdMonogram');
+    showElem('cdPersonIconAdd');
+}
+
+function setFormAdd() {
+    // setInputsAdd();
+    setButtonsAdd();
+    // addValidation('contactForm', validateContactExisting, 'emailVmsg', 'You already added a contact with this email');
+    // removeSubmitHandler('contactForm', submitHandlerEdit);
+    addSubmitHandler('contactForm', submitHandlerAdd);
+}
+
+function setButtonsAdd() {
+    let submitButtonText= document.getElementById('CDsubmitButtonText');
+    submitButtonText.innerHTML= 'Create contact';
+    hideElem('cdDeleteButton');
+    showElem('cdCancelButton');
+}
+
+function submitHandlerAdd() {
+    console.log('submitHandlerAdd()'); ///DEBUG
+}
+
 /*###########*/
 /*## MEDIA ##*/
 /*###########*/
@@ -55,7 +110,7 @@ function setInfo() {
 function mediaChangeHandler() {
     if (mediaDesktop.matches) {
         showElem('detailContainer');
-        hideContactInfoButtonMobile();
+        // hideContactInfoButtonMobile();
     }
     else hideElem('detailContainer');
 }
