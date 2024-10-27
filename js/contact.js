8a38eb3553974c0cfd329c5efbfea81968f24772
MetalRick <lernen@reinitestet.at>
2024-10-27 06:34:55 +0100

    add Contactlist-Item Clickhandler

--- js/contact.js
+++ js/contact.js
@@ -3,4 +3,20 @@
 function initContact() {
     console.log('intitContact()'); ///DEBUG
     include();
+    addListItemClickHandlers();
+}
+
+function addListItemClickHandlers() {
+    let listItems= document.querySelectorAll('.conlist-item'); 
+    for (itemI of listItems) {
+        itemI.addEventListener('click', listItemClickHandler);
+    }
+}
+
+/*##################*/
+/*## CONTACT LIST ##*/
+/*##################*/
+
+function listItemClickHandler() {
+    console.log('listItemClickHandler()'); ///DEBUG
 }
