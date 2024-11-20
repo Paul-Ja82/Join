f426d57b871b1cc24252aee477793e66ecefb05d
MetalRick <lernen@reinitestet.at>
2024-11-20 19:08:13 +0100

    hä?

--- js/database-reini.js
+++ js/database-reini.js
@@ -1,12 +1,13 @@
 
 // Firebase-URL von Sabrina
-const BASE_URL = "https://join-7ae0e-default-rtdb.europe-west1.firebasedatabase.app/";
+// const BASE_URL = "https://join-7ae0e-default-rtdb.europe-west1.firebasedatabase.app/";
 
 // Firebase-URL von Reini
-// const BASE_URL = "https://join-43c13-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://join-43c13-default-rtdb.europe-west1.firebasedatabase.app/";
 
 const USERS_PATH = 'users/';
-const CONTACTS_PATH= 'contacts/'
+// const CONTACTS_PATH= 'contacts/'
+const CONTACTS_PATH= 'contactsTESTING/'; ///DEBUG
 const NEXT_ID_PATH = 'nextId/';
 
 
