561147031d5bb011dab472e393ee84b21ac83b3f
Paul J. Dietrich <pallone33@gmail.com>
2024-12-02 20:01:55 +0100

    new_password.js mit jsDoc fertig

--- js/new_password.js
+++ js/new_password.js
@@ -4,6 +4,10 @@
 /*## INIT ##*/
 /*##########*/
 
+/**
+ * Initializes the new password setup by loading user data, configuring forms,
+ * and adding validation and submit handlers.
+ */
 function initNewPassword() {
     loadUsers()
         .then(loadCurrentUser);
@@ -13,29 +17,43 @@ function initNewPassword() {
     addSubmitHandler('formNewPw', afterPwChangeHandler);
 }
 
+/**
+ * Checks if the new password and confirmation password match.
+ * @returns {boolean} True if the passwords match, otherwise false.
+ */
 function isPasswordConfirm() {
-    let pw= document.getElementById('newPassword').value;
-    let pwConfirm= document.getElementById('passwordInput').value;
+    let pw = document.getElementById('newPassword').value;
+    let pwConfirm = document.getElementById('passwordInput').value;
     return pw == pwConfirm;
 }
 
+/**
+ * Loads the current user based on the ID retrieved from the URL parameters.
+ */
 function loadCurrentUser() {
     let params = new URLSearchParams(document.location.search);
-    currentUser= getUserById(params.get('id'));
-    console.log(currentUser); ///DEBUG
+    currentUser = getUserById(params.get('id'));
+    console.log(currentUser); // DEBUG
 }
+
 
 /*#####################*/
 /*## UPDATE PASSWORD ##*/
 /*#####################*/
 
+/**
+ * Updates the user's password by saving the new password to the specified storage path.
+ */
 function updateUserPw() {
-    let newPassword= document.getElementById('newPassword').value;
-    let path= USERS_PATH + `${currentUser.id}/${'pw'}`;
-    console.log(path); ///DEBUG
+    let newPassword = document.getElementById('newPassword').value;
+    let path = USERS_PATH + `${currentUser.id}/${'pw'}`;
     saveData(path, newPassword);
 }
 
+/**
+ * Handles post-password change actions by displaying a confirmation toast
+ * and redirecting the user to the index page.
+ */
 function afterPwChangeHandler() {
-    showToast('pwChangeToast', ()=> loadPage(INDEXPAGE_URLS[0]));
+    showToast('pwChangeToast', () => loadPage(INDEXPAGE_URLS[0]));
 }
