914c137411465a5dd6a1c30736f369bb6e1a72a4
Paul J. Dietrich <pallone33@gmail.com>
2024-12-10 15:27:34 +0100

    script.js komentar bloks removed end jsCode comlpeted

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
-
-/*##########*/
-/*## INIT ##*/
-/*##########*/
-
 let currentUser;
 
+/**
+ * Initializes the join functionality by setting up MPA, dialogs, and loading contacts.
+ * @async
+ * @function
+ */
 async function initJoin() {
   initMPA();
   initDialog();
-  // await loadCurrentUser();
   await loadContacts();
 }
 
+/**
+ * Loads contact data, processes it into an array, and sorts it by name.
+ * @async
+ * @function
+ */
 async function loadContacts() {
   contacts = [];
   let path = CONTACTS_PATH;
@@ -23,50 +26,74 @@ async function loadContacts() {
   sortContactsByName();
 }
 
+/**
+ * Loads the currently logged-in user's data.
+ * @async
+ * @function
+ */
 async function loadCurrentUser() {
   let userId = getLoggedIn();
   let path = USERS_PATH + userId;
   currentUser = await getData(path);
 }
 
-/*##############*/
-/*## CONTACTS ##*/
-/*##############*/
-
 let contacts = [];
 
+/**
+ * Retrieves a contact by its ID.
+ * @function
+ * @param {number|string} id - The ID of the contact.
+ * @returns {Object|undefined} The contact object or undefined if not found.
+ */
 function getContactById(id) {
   return contacts.find((contactI) => contactI.id == id);
 }
 
+/**
+ * Retrieves a contact by its email address.
+ * @function
+ * @param {string} email - The email of the contact.
+ * @returns {Object|undefined} The contact object or undefined if not found.
+ */
 function getContactByEmail(email) {
   return contacts.find((contactI) => contactI.email == email);
 }
 
-function getPersonIcon(id) {
+/**
+ * Placeholder for obtaining a person's icon.
+ * @function
+ * @param {number|string} id - The ID of the person.
+ */
+function getPersonIcon(id) {}
 
-}
-
+/**
+ * Sorts the contacts array alphabetically by name.
+ * @function
+ */
 function sortContactsByName() {
   contacts.sort((contactI, contactJ) => contactI.name.localeCompare(contactJ.name));
 }
 
-/*##########*/
-/*## MISC ##*/
-/*##########*/
-
+/**
+ * Gets the first letter of a given string.
+ * @function
+ * @param {string} text - The input string.
+ * @returns {string} The first letter of the string.
+ */
 function getFirstLetter(text) {
   return text.charAt(0);
 }
 
+/**
+ * Gets the next ASCII character of the first letter of a string.
+ * @function
+ * @param {string} text - The input string.
+ * @returns {string} The next ASCII character in uppercase.
+ */
 function getNextASCIIchar(text) {
-  let char= getFirstLetter(text).toUpperCase();
+  let char = getFirstLetter(text).toUpperCase();
   return String.fromCharCode(char.charCodeAt(0) + 1).toUpperCase();
 }
-
-/*###########*/
-/*## LOGIN ##*/
-/*###########*/
 
 /**
  * Handles the masking of password input fields by updating the visible input value
@@ -208,37 +235,40 @@ function eyeLockVariations(input, lockImg, eyeOffImg, eyeOnImg) {
   }
 }
 
-
-
-
-
-
-
-
-/*###########*/
-/*## DEBUG ##*/
-/*###########*/
-
+/**
+ * Logs all users to the console.
+ * @function
+ */
 function logUsers() {
   getData(USERS_PATH)
     .then(users => console.log(users));
 }
 
+/**
+ * Deletes a user by their ID and logs the updated list of users.
+ * @function
+ * @param {number|string} userId - The ID of the user to delete.
+ */
 function deleteUser(userId) {
-  let path= USERS_PATH + '/' + userId;
+  let path = USERS_PATH + '/' + userId;
   deleteData(path)
     .then(logUsers);
 }
 
+/**
+ * Toggles the display of user message and main frames.
+ * @function
+ */
 function toggleDisplay() {
   const userMsgFrame = document.querySelector('.user-msg-frame');
   const mainFrameDiv = document.querySelector('.main-frame-div');
 
   if (userMsgFrame) {
-      userMsgFrame.style.display = 'none';
+    userMsgFrame.style.display = 'none';
   }
 
   if (mainFrameDiv) {
-      mainFrameDiv.style.display = 'block';
+    mainFrameDiv.style.display = 'block';
   }
 }
+
