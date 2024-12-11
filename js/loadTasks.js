696c4dcd942178e017d1d12a7f1378144b51149a
Zombie84 <zombie88@gmx.at>
2024-12-11 02:57:46 +0100

    delete comment in loadTasks.js

--- js/loadTasks.js
+++ js/loadTasks.js
@@ -1,6 +1,4 @@
-const firebase_URL = 'https://join-7ae0e-default-rtdb.europe-west1.firebasedatabase.app/'
-// const firebase_URL = 'https://join-43c13-default-rtdb.europe-west1.firebasedatabase.app/'
-
+const firebase_URL = 'https://join-7ae0e-default-rtdb.europe-west1.firebasedatabase.app/';
 let allTasks = [];
 let allKeys = [];
 let allContactsForTasks = [];
